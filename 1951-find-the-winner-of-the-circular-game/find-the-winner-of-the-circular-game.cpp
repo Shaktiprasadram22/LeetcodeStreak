876cf754335aed170a1d6bce0570eaaed0e11060
Shakti Prasad ram <108186897+Shaktiprasadram22@users.noreply.github.com>
2025-02-11 13:54:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (82.50%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
-    int findTheWinner(int n, int k) {
-        if (n == 1) {
-            return 1; // Return 1-based index for the winner
+
+    int winner(int n , int k){
+        if(n==1){
+            return 0;
+
         }
-        return (findTheWinner(n - 1, k) + k - 1) % n +
-               1; // Adjust for 1-based indexing
+        return (winner(n-1,k)+k)%n;
+    }
+    int findTheWinner(int n, int k) {
+
+        return winner(n , k)+1;
+        
     }
 };
